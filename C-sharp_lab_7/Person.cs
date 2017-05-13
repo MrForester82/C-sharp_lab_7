abd9310c508870a3053495a30b9f2edc4f5f6ca1
MrForester82 <potate82@ya.ru>
2017-05-13 20:33:30 +0300

    1 - Задание  1 (свойства)

---
+++ C-sharp_lab_7/Person.cs
@@ -0,0 +1,56 @@
+﻿
+namespace C_sharp_lab_7
+{
+    class Person
+    {
+        private string fam;
+        private string status;
+        private float salary;
+        private int age;
+
+        public string Fam      //read, write-once
+        {
+            //set { if (fam == "") fam = value; }
+            get { return fam; }
+        }
+        public string Status   //read-only
+        {
+            set { if(status == "") status = value; }
+            get { return status; }
+        }
+        public float Salary    //write-only
+        {
+            set { salary = value; }
+        }
+        public int Age         //read, write
+        {
+            set { age = value; }
+            get { return age; }
+        }
+        public string health;  //not-read, not-write
+
+
+        public Person(string fam, string status)
+        {
+            this.fam = fam;
+            this.status = status;
+        } 
+
+        public void setHealth(string val)
+        {
+            health = val;
+        }
+
+        public string getHealth()
+        {
+            return health;
+        }
+        /*  Не допускается компилятором
+        public float getSalary()
+        {
+            return salary;
+        }
+        */
+    }
+}
+
--- C-sharp_lab_7/Program.cs
+++ C-sharp_lab_7/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_sharp_lab_7
 {
@@ -10,6 +6,17 @@ namespace C_sharp_lab_7
     {
         static void Main(string[] args)
         {
+            Person gregor = new Person("Dorryson", "student");
+
+            gregor.Age = 22;
+            gregor.Salary = 89000.00F;
+            gregor.setHealth("like a rhino");
+
+            Console.WriteLine(gregor.Fam);
+            Console.WriteLine(gregor.Age);
+            Console.WriteLine(gregor.Status);
+            Console.WriteLine(gregor.getHealth());
+            //Console.WriteLine(gregor.getSalary());
 
         }
     }
